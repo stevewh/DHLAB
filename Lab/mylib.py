826d41b6079a5205b835ae0efa179ec7707a7dcf
Stephen White <stephenawhite57@gmail.com>
2019-11-20 15:17:11 +0100

    organizing code

---
+++ Lab/mylib.py
@@ -0,0 +1,20 @@
+print ("Loading MYLIB library")
+
+def square(x):
+    """
+      Square the input number
+      Args:
+         x: input number
+      Returns:
+         the result of multiplying the number by itself
+    """
+    return x**2 # raise input to the second power
+
+def cube(x):
+    return x**3 #cude the x value
+
+def pwr(x,y):
+    return x**y #raise x to the power of y
+
+def testFunction():
+    print("mylib module tested ok2")
